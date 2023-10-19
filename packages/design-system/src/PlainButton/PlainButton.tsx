8c857bed0ab3bb04be38e87456efc46d7d0b4797
Alfredo Guida <guida.alfredo@gmail.com>
2023-10-19 12:06:56 +0200

    type fix

---
+++ packages/design-system/src/PlainButton/PlainButton.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react'
+import { Button } from '@mui/material'
+
+const Index: FunctionComponent = () => {
+  return <Button>Hello Works</Button>
+
+}
+
+export default Index
---
+++ packages/design-system/src/PlainButton/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './PlainButton'
+export * from './PlainButton'
